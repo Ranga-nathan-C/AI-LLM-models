30dfecae9fb179293eec9354f77485f14a9f5ebf
Renganathanc <nathancranga4@gmail.com>
2024-04-05 15:37:48 +0530

    Deploy3

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,14 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import Home from "../Home";
 import Home2 from "../Home2";
+import Login from "../Login";
+import Signup from "../SIgnup";
 import Favouite from "../Favourite";
 
 const router = createBrowserRouter([
-  
   {
     path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/Home",
     element: <Home2 />,
   },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/Signup",
+    element: <Signup />,
+  },
+
   {
     path: "/Favourite",
     element: <Favouite />,
